cb5d06fb10186c2499d14942b2db8fcaf2b83eb4
JuntosWeb <163202694+JuntosWeb@users.noreply.github.com>
2026-02-11 17:26:28 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
 
 async function bootstrap() {
   const port = process.env.PORT ?? 3000
   const apiHost = process.env.API_HOST ?? 'http://localhost:3000'
   const app = await NestFactory.create(AppModule);
+  const corsConfig: CorsOptions = {
+    origin: 'https://juntosporlosdemas.org',   // <- tu dominio exacto
+    methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
+    allowedHeaders: [
+      'Content-Type',
+      'Authorization',
+      'Accept',
+      'X-Requested-With',
+    ],
+    credentials: true,                         // si usas cookies o auth headers
+    maxAge: 86400,                             // cache pre‑flight 24h (opcional)
+  };
   console.log(`running on port: ${port}`);
   console.log(`API_HOST: ${apiHost}`);
-  app.enableCors()
+  app.enableCors(corsConfig);
   await app.listen(port);
 }
 bootstrap();
