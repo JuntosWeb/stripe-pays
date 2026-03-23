c1bb3c0d6f0d8d4d6c51e614f03da67b9f402b6c
JuntosWeb <163202694+JuntosWeb@users.noreply.github.com>
2026-03-23 17:07:51 -0600

    Update CORS

--- src/main.ts
+++ src/main.ts
@@ -9,13 +9,13 @@ async function bootstrap() {
   const corsConfig: CorsOptions = {
     origin: ['https://juntosporlosdemas.org','https://www.juntosporlosdemas.org'],  
     methods: ['GET', 'POST', 'OPTIONS', 'PUT', 'PATCH', 'DELETE'],
-    allowedHeaders: '*',
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,                         
     maxAge: 86400,                             
   };
   console.log(`running on port: ${port}`);
   console.log(`API_HOST: ${apiHost}`);
-  app.enableCors({...});
+  app.enableCors(corsConfig);
   await app.listen(port);
 }
 bootstrap();
